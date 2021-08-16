924e963a6b831470eb1abbf104b22e24d04067ca
Besart Zeqiri <bz47921@ubt-uni.net>
2021-08-16 22:20:13 +0200

    Lidhja me firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCisGCn3xsSe-pGsOaIW8C8H8XJxq08fEM",
+  authDomain: "fir-7bfaa.firebaseapp.com",
+  projectId: "fir-7bfaa",
+  storageBucket: "fir-7bfaa.appspot.com",
+  messagingSenderId: "460519757840",
+  appId: "1:460519757840:web:a40ef862f5c720627b9295",
+  measurementId: "G-13P8PQD5ZE"
+};
