6d4d7deff382c6f34b9ed6129019231839c76bae
Besart Zeqiri <bz47921@ubt-uni.net>
2021-08-21 21:23:02 +0200

    Perdorimi i react-router-dom

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <div className='header'>
+            <Link to="/">
             <img
             className="header__logo"
              src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+            </Link>
+            
             
             <div className="header__search">
                 <input className="header__searchInput" type="text"/>
@@ -44,6 +48,7 @@ function Header() {
                       Prime
                     </span>
                </div>
+               <Link to="/checkout">
                <div className="header__optionBasket">
                    <ShoppingBasketIcon/>
                    <span className=" header__optionLineTwo header__basketCount">
@@ -51,6 +56,8 @@ function Header() {
                      </span>
 
                </div>
+               </Link>
+               
           
             </div>
 
--- src/Home.js
+++ src/Home.js
@@ -10,18 +10,48 @@ function Home() {
                  alt=""/>
 
                  <div className="home__row">
-                     <Product/>
-                     <Product/>
+                     <Product 
+                     id="12345611"
+                     title='Apple iPhone 12 Pro Max, 128GB, Gold - Fully Unlocked (Renewed)'
+                     price={1350} 
+                     image='https://m.media-amazon.com/images/I/71u1BzjGJbL._AC_SL1500_.jpg'
+                     rating={4}/>
+                     <Product
+                     id="49538094"
+                     title="DualShock 4 Wireless Controller for PlayStation 4 - Jet Black"
+                     price={59.99}
+                     image="https://m.media-amazon.com/images/I/61IG46p-yHL._SL1500_.jpg"
+                     rating={4}/>
                  </div>
 
                  <div className="home__row">
-                     <Product/>
-                     <Product/>
-                     <Product/>
+                     <Product
+                     id="1111111"
+                     title="Apple EarPods with Lightning Connector - White"
+                     price={40.99}
+                     image="https://m.media-amazon.com/images/I/41wYbyr3LLL._AC_SL1144_.jpg"
+                     rating={4}/>
+                     <Product
+                     id="222222"
+                     title="ASUS ROG Zephyrus S Ultra Slim Gaming PC Laptop"
+                     price={1200}
+                     image="https://m.media-amazon.com/images/I/81Nox3hzQaL._AC_SL1500_.jpg"
+                     rating={5}/>
+                     <Product
+                     id="3333333"
+                     title="Acer Nitro 5 Gaming Laptop"
+                     price={769.99}
+                     image="https://m.media-amazon.com/images/I/71s1LRpaprL._AC_SL1500_.jpg"
+                     rating={4}/>
                  </div>
 
                  <div className="home__row">
-                 <Product/>
+                 <Product
+                 id="44444444"
+                 title="SAMSUNG Galaxy Z Flip 3 5G"
+                 price={999.99}
+                 image="https://m.media-amazon.com/images/I/51JgTGwH8US._AC_SL1000_.jpg"
+                 rating={4}/>
                   </div>
 
             </div>
