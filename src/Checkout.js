59c6b6e38f32e2070571718618d461f2a0cdb2c2
Besart Zeqiri <bz47921@ubt-uni.net>
2021-08-23 09:49:42 +0200

    disajnimi i subtotals

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
     return (
@@ -21,8 +22,7 @@ function Checkout() {
 
             </div>
             <div className="checkout__right">
-                <Subtotal/>
-                <h2>The subtotal will go here</h2>
+                <Subtotal />
 
             </div>
         </div>
