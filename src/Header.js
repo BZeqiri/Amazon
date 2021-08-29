47725add28694ac6495bbeb61f1be07a660aca55
Besart Zeqiri <bz47921@ubt-uni.net>
2021-08-29 22:11:51 +0200

    krijimi i login page (front-end)

--- src/Header.js
+++ src/Header.js
@@ -21,6 +21,7 @@ function Header() {
                 <SearchIcon className="header__searchIcon"/>
             </div>
             <div className="header__nav">
+                <Link to='/login'>
                 <div className="header__option">
                     <span className="header__optionLineOne">
                         Hello Guest
@@ -28,9 +29,8 @@ function Header() {
                     <span className="header__optionLineTwo">
                        Sign In
                     </span>
-
-
                 </div>
+                </Link>
                 <div className="header__option">
                 <span className="header__optionLineOne">
                         Returns
