3aea51011a9727edeccb290a9523c2b81b49e5e2
Besart Zeqiri <bz47921@ubt-uni.net>
2021-10-10 23:31:20 +0200

    Krijimi i payment page

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -4,8 +4,10 @@ import CurrencyFormat from "react-currency-format";
 import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
 import {useStateValue} from "./StateProvider";
 import {getBasketTotal} from "./reducer";
+import { useHistory } from 'react-router';
 
 function Subtotal() {
+    const history = useHistory();
     const [{basket}, dispatch] = useStateValue();
     return (
         <div className="subtotal">
@@ -30,7 +32,7 @@ function Subtotal() {
               thousandSeparator={true}
               prefix={"$"}
               />
-              <button>Proceed to Checkout</button>
+              <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
         </div>
     )
 }
